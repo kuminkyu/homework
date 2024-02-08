0e7945093ed29ff2290ac4fbbf446be08ab38baf
kuminkyu <kuminkyu@datasolution.kr>
2024-02-08 14:16:18 +0900

    Sub Branch Second Commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int sum = 0;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i <= 100; i++) {
 
             if (i % 3 != 0 && i % 5 != 0) {
+                sum += i;
+                System.out.println(i);
+            }else{
                 sum += i;
                 System.out.println(i);
             }
