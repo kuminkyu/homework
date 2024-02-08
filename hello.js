4eb3590e96e7b630bc74cb92eb145a0e69735d60
kuminkyu <kuminkyu@datasolution.kr>
2024-02-08 14:12:58 +0900

    First Commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+function max(arg1, arg2){
+    var arglisLarger = arg1>arg2;
+    console.log(arglisLarger);
+    if(arglisLarger){
+        return arg1;
+    }else{
+        return arg2;
+    }
+}
+
+class Parent{
+
+}
+class Child extends Parent{
+
+}
