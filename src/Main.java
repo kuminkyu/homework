17547bc2542b096b55ee522f4486afed5fa65f9b
kuminkyu <kuminkyu@datasolution.kr>
2024-02-08 14:14:33 +0900

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = 0;
-
+        int sum1 = 0;
+        int sum2 = 0;
         for (int i = 0; i < 100; i++) {
 
             if (i % 3 != 0 && i % 5 != 0) {
-                sum += i;
-                System.out.println(i);
+                sum1 += i;
+            }else{
+                sum2 += i;
             }
         }
-        System.out.println(sum);
-
+        System.out.println(sum1);
+        System.out.println(sum2);
     }
 }
